719ad2a898b5342fb07fd2bafdd7af141a17513e
Nykolai Kuteinykov <nykolai.kuteinykov@i.ua>
2019-12-27 23:58:28 +0200

    refactoring DayWeek

--- src/ua/skillsup/DayWeek.java
+++ src/ua/skillsup/DayWeek.java
@@ -1,7 +1,8 @@
 package ua.skillsup;
 
 public enum DayWeek {
-    MONDAY(1), TUESDAY(1), WEDNESDAY(1), THURSDAY(1), FRIDAY(3),
+    MONDAY(1), TUESDAY(1), WEDNESDAY(1), THURSDAY(1),
+    FRIDAY(3),
     SATURDAY(2), SUNDAY(2);
 
     private int idDayType;
